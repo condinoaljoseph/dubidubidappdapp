9596e8cf64074339382a6362b16568e07c48f1a0
Al Joseph Condino <condinoaljoseph@gmail.com>
2022-03-25 19:35:05 +0800

    removed comment

--- src/lens/authentication/authenticate.ts
+++ src/lens/authentication/authenticate.ts
@@ -21,25 +21,3 @@ export const authenticate = (address: string, signature: string) => {
     },
   });
 };
-
-// export const login = async (address: string) => {
-// //   if (getAuthenticationToken()) {
-// //     console.log('login: already logged in');
-// //     return;
-// //   }
-
-//   console.log('login: address', address);
-
-//   // we request a challenge from the server
-//   const challengeResponse = await generateChallenge(address);
-
-//   // sign the text with the wallet
-//   const signature = await signText(challengeResponse.data.challenge.text);
-
-//   const accessTokens = await authenticate(address, signature);
-//   prettyJSON('login: result', accessTokens.data);
-
-//   setAuthenticationToken(accessTokens.data.authenticate.accessToken);
-
-//   return accessTokens.data;
-// };
